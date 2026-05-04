baeda8b7ba23a0757b2be7826549d026c60d118b
Cesar Gomez <estiveg576@gmail.com>
2026-05-03 20:36:19 -0500

    fix: seguridad - SQL injection, CSRF, password_hash, gitignore y mejoras BD

--- Config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-const base_url = "http://localhost/biblioteca/";
-const host = "localhost";
-const user = "root";
-const pass = "";
-const db = "biblioteca";
-const charset = "charset=utf8";
-?>
